06f714eb4a5ec895eccf1ecbfa38f040d01a0ea8
Igor Manassypov <imanassypov@gmail.com>
2019-03-20 10:53:11 -0400

    -collapsed CUSIP id into contiguous string -included header definitions -added extraction of OCR'ed number of records from the end of the file -last row from extraction is dropped, which contained expected number of records

--- pyurlscrubfed.py
+++ pyurlscrubfed.py
@@ -8,6 +8,8 @@ import PyPDF2
 import tabula
 import pandas as pd
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import re 
+
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 # TABULA REF: https://github.com/chezou/tabula-py
@@ -31,7 +33,8 @@ PG_HEIGHT=600
 PG_AREA=(PG_TOP,PG_LEFT,PG_TOP+PG_HEIGHT,PG_LEFT+PG_WIDTH)
 #COLUMN DIVIDORS, ALSO MEASURE USING OSX PREVIEW
 #TABLE OUTER BOUNDARIES ARE NOT TO BE SPECIFIED
-PG_COLS=(111.62,129.81,143.76,163.71,346.4,465.47)
+#PG_COLS=(111.62,129.81,143.76,163.71,346.4,465.47) #First two divs separate out the cusip into three pieces
+PG_COLS=(143.76,163.71,346.4,465.47) #First two divs separate out the cusip into three pieces
 # WHEN DEFINIING AREA SWITCH OFF AUTO-DETECTION, IE GUESS=OFF
 
 #START SCANNING FROM PAGE
@@ -39,6 +42,9 @@ PG_START_INDEX = 3
 
 #JAVA Options
 JAVA_OPTS='-Xmx2G'
+
+#imported table headers
+TBL_HEADER=["CUSIP NO","-","ISSUER NAME", "ISSUER DESCRIPTION", "STATUS"]
 
 page = requests.get('https://www.sec.gov/divisions/investment/13flists.htm')
 
@@ -59,16 +65,27 @@ for f in file_items:
     filename = link.rsplit('/', 1)[-1]
     print ("PDF file link:\t", link)
     print ("PDF filename:\t", filename)
-    try:
-        print ("Grabbing file...")
-        r = requests.get(link, allow_redirects=True)
-        open (filename, 'wb').write(r.content)
-        pdfFObj = open (filename, 'rb')
-        pdfReader = PyPDF2.PdfFileReader(pdfFObj)
-        num_pages = pdfReader.numPages
+    print ("Grabbing file...")
+    print (link)
+    r = requests.get(link, allow_redirects=True)
+    open (filename, 'wb').write(r.content)
+    pdfFObj = open (filename, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFObj)
+    num_pages = pdfReader.numPages
 
-        print ("Extracting pages:\t", start_page, '-', num_pages)
-        tabula.convert_into(filename, filename + '.csv', output_format="csv", pages= str(start_page) + '-' + str(num_pages),area=PG_AREA,columns=PG_COLS,guess=False,java_options=JAVA_OPTS)
-        success = True
-    except Exception  as e:
-        print('exception \t', e)
+    print ("Extracting pages:\t", start_page, '-', num_pages)
+    #tabula.convert_into(filename, filename + '.csv', output_format="csv", pages= str(start_page) + '-' + str(num_pages),area=PG_AREA,columns=PG_COLS,guess=False,java_options=JAVA_OPTS)
+    df = tabula.read_pdf(filename,pages= str(start_page)+'-'+str(num_pages),area=PG_AREA,columns=PG_COLS,guess=False,java_options=JAVA_OPTS,pandas_options={'error_bad_lines':False, 'names':TBL_HEADER})
+    #last line contains total count as imported from pdf
+    expected_row_count = int("".join(re.findall(r'\d',(str(df.iloc[len(df)-1][len(TBL_HEADER)-1])))))
+    print ("Expected row count:\t{ROWCOUNT}".format(ROWCOUNT=expected_row_count))
+
+    #drop last row as it is just a note on expected number of entries
+    df.drop(df.index[len(df)-1],axis=0,inplace=True)
+
+    extracted_row_count = len(df)
+    print ("Extracted row count:\t{ROWCOUNT}".format(ROWCOUNT=extracted_row_count))
+
+    df.to_excel(filename+'.xlsx',index=False,header=True)
+    success = True
+
